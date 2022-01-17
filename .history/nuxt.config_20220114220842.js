381337c666f7e60dcefe19c0b3326ed67100b71c
hashiz008 <hashiz0314@icloud.com>
2022-01-17 23:30:38 +0900

    ７６９px以上のレスポンシブを大部分を修正しました。

---
+++ .history/nuxt.config_20220114220842.js
@@ -0,0 +1,6 @@
+module.exports = {
+    plugins: [{ src: "@/plugins/vue-awesome-swiper", ssr: false }],
+    build: {
+        vendor: ["vue-awesome-swiper"],
+    },
+};
---
+++ .history/vue.config_20220115120843.js
@@ -0,0 +1,6 @@
+module.exports = {
+    plugins: [{ src: '../plugins/', ssr: false }],
+    build: {
+        vendor: ["vue-awesome-swiper"],
+    },
+};
